ffc120023fe12a155872c859f1d4f7fb95eadeab
bigy359 <yuichi.big@gmail.com>
2022-02-24 18:03:11 +0900

    lesson 0224

--- src/index.js
+++ src/index.js
@@ -37,8 +37,8 @@
 /**
  * テンプレート文字列
  */
-const name = "ジャケ";
-const age = 28;
+// const name = "ジャケ";
+// const age = 28;
 //私の名前はジャケです。年齢は28です。
 
 //従来方法
@@ -52,24 +52,54 @@ const age = 28;
 /**
  * アロー関数
  */
-//従来方法
-function func1(str) {
-  return str;
-}
-console.log(func1("func1です"));
+// //従来方法
+// function func1(str) {
+//   return str;
+// }
+// console.log(func1("func1です"));
 
 //アロー関数
-const func2 = (str) => {
-  return str;
-};
-console.log(func2("func2です"));
+// const func2 = (str) => {
+//   return str;
+// };
+// console.log(func2("func2です"));
 
-const func2_2 = (str) => str;
-console.log(func2_2("func3です"));
+// const func2_2 = (str) => str;
+// console.log(func2_2("func3です"));
 
-const func3 = (num1, num2) => {
-  return num1 + num2;
-};
-console.log(func3(3, 4));
+// const func3 = (num1, num2) => {
+//   return num1 + num2;
+// };
+// console.log(func3(3, 4));
 
-console.log(func3(30, 4));
+// console.log(func3(30, 4));
+
+/**
+ * 分割代入
+ */
+
+// const myprofile = {
+//   name: "ジャケ",
+//   age: 28,
+//   }
+// //私の名前はジャケです。年齢は28です。
+
+// //従来方法
+// const message1 = `私の名前は${myprofile.name}です。年齢は${myprofile.age}です。`;
+// console.log(message1);
+
+// //分割代入
+// const { name, age} = myprofile;
+// const message2 = `名前は${name}です。年齢は${age}です`
+
+// const myprofile = ["ジャケ", 28];
+// const [name,age] = myprofile;
+// const message3 = `私の名前は${name}で年齢は${age}です`;
+// console.log(message3);
+
+/**
+ * デフォルト値
+ */
+
+//  const sayhello = (name = "テスト") => console.log(`こんにちは${name}さん`);
+//  sayhello()
